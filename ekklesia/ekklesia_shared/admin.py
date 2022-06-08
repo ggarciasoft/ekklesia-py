562acdc22f9b0274e95298123785c0bc048a7204
Gregory Garcia <gregory.garcia@authoritypartners.com>
2022-06-07 20:20:03 -0400

    Insert user and update user

--- ekklesia/ekklesia_shared/admin.py
+++ ekklesia/ekklesia_shared/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from datetime import datetime
 # Register your models here.
 from .models import EntityType, Organization
 from .models import ActivityType
@@ -11,6 +11,13 @@ class CommonTypeAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['alias', 'name', 'description', 'is_active']})
     ]
+    def save_model(self, request, obj, form, change):
+        if obj.id > 0:
+            obj.update_user = request.user
+            obj.update_date = datetime.now()
+        else:
+            obj.insert_user = request.user
+        obj.save()
 
 admin.site.register(EntityType, CommonTypeAdmin)
 admin.site.register(ActivityType, CommonTypeAdmin)
